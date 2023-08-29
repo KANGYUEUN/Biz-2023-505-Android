3afe6f2f1e21d3ff03dc86680349121778d68294
KANGYUEUN <yu1128eun@kakao.com>
2023-08-29 09:29:15 +0900

    0829

--- flutter-000-hello/lib/hello.dart
+++ flutter-000-hello/lib/hello.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  const text = Text(
-    "반갑습니다",
-    style: TextStyle(
-      fontSize: 30,
-      color: Color.fromRGBO(255, 0, 0, 1),
-    ),
-    textDirection: TextDirection.ltr,
-  );
+  const text = NewWidget();
   runApp(const Center(
     child: text,
   ));
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "반갑습니다",
+      style: TextStyle(
+        fontSize: 30,
+        color: Color.fromRGBO(255, 0, 0, 1),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+  }
 }
---
+++ flutter.vsCode.setting.md
@@ -0,0 +1,7 @@
+# vsCode Flutter 개발 환경
+
+- extention 설치 ![확장설치](image.png)
+- 자동완성 setting : 설정(Ctrl + , ) 창 에서 ![codeAction](image-1.png) On Save 에서 settings.json편집 클릭
+- ![source.fixAll:true](image-2.png) 두줄 설정하기
+
+- widget tree guide 추가 하기 : ![guide](image-3.png) `Dart preview flutter UI Guide` 체크
